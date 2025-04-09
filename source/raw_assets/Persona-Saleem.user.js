2257bac4d38439213781f0cd71f634a2af4f93e8
Anika Henke <anika.henke@digital.cabinet-office.gov.uk>
2025-04-09 13:22:46 +0100

    Move translate-to-piglatin.user.js content into new userscript for Saleem

---
+++ source/raw_assets/Persona-Saleem.user.js
@@ -0,0 +1,100 @@
+// ==UserScript==
+// @name         Persona Saleem
+// @namespace    https://github.com/alphagov/accessibility-personas
+// @version      1.0.0
+// @license      MIT
+// @author       Chazona Baum and Crown Copyright (Government Digital Service)
+// @description  Saleem, a profoundly deaf user - everything on the page is translated into Pig Latin to simulate English as a second language
+// @homepageURL  https://alphagov.github.io/accessibility-personas/
+// @include      *
+// @grant        none
+// @nocompat     Chrome
+// ==/UserScript==
+
+/**
+ * Translate to Pig Latin
+ * @author Chazona Baum [https://codepen.io/chznbaum/pen/GNNwJZ]
+ * @author Crown Copyright (Government Digital Service)
+ * @license MIT
+ */
+
+(function() {
+  'use strict';
+
+  function translateWord(str) {
+    var wordArray = str.split('');
+    var vowels = 'AaEeIiOoUu';
+    var consonant;
+
+    for (var i = 0; i < wordArray.length; i++) {
+      var wordStartsWithVowel = vowels.includes(wordArray[0]);
+      var wordStartsWithVowelOrY = wordStartsWithVowel || 'Yy'.includes(wordArray[0]);
+
+      if (wordStartsWithVowel) {
+        str = wordArray.join('');
+        if (i === 0) {
+          str = str + 'way';
+        } else {
+          str = str + 'ay';
+        }
+        return str;
+      } else if (i !== 0 && wordStartsWithVowelOrY) {
+        str = wordArray.join('');
+        str = str + 'ay';
+        return str;
+      } else {
+        consonant = wordArray.shift();
+        wordArray.push(consonant);
+      }
+    }
+    return str;
+  }
+
+  function translatePhrase(str) {
+    var originalString = str;
+    var originalArray = originalString.split(' ');
+    var translatedArray = originalArray.map(function(word) {
+      word = translateWord(word);
+
+      for (var i = 0; i < word.length; i++) {
+        if (!word[i].match(/[a-z]/i) && (i < word.length)) {
+          var punctuation = word[i];
+          var punctuatedWordArray = word.split('');
+          var removedPunctuation = punctuatedWordArray.splice(i, 1);
+          if (i === 0) {
+            word = punctuation + punctuatedWordArray.join('');
+          } else if (i !== 0) {
+            word = punctuatedWordArray.join('') + punctuation;
+          }
+        }
+
+        if (word.charCodeAt(i) >= 65 && word.charCodeAt(i) <= 90) {
+          word = word.toLowerCase();
+          var firstLetter = word.slice(0, 1);
+          var capLetter = firstLetter.toUpperCase();
+          word = word.replace(firstLetter, capLetter);
+        }
+      }
+      return word;
+    });
+    return translatedArray.join(' ');
+  }
+
+  function translatePage() {
+    var walker = document.createTreeWalker(
+      document.body,
+      NodeFilter.SHOW_TEXT,
+      null,
+      false
+    );
+
+    while (walker.nextNode()) {
+      if (walker.currentNode.nodeValue.trim()) {
+        walker.currentNode.nodeValue = translatePhrase(walker.currentNode.nodeValue);
+      }
+    }
+  }
+
+  translatePage();
+
+})();
